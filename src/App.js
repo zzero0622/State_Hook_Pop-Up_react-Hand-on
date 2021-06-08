0a20972ae475998b7225e934a811664cd56d4f0e
zzero0622 <81890240+zzero0622@users.noreply.github.com>
2021-06-08 15:15:29 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = (e) => {
+    // Pop up 의 open/close 상태에 따라
+    // 현재 state 가 업데이트 되도록 함수를 완성하세요.
+    setShowPopup(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Fix me to open Pop Up</h1>
+      {/* 버튼을 클릭했을 때 Pop up 의 open/close 가 작동하도록
+          button tag를 완성하세요. */}
+      <button className="open" onChange={togglePopup}>
+        Open me
+      </button>
+      {showPopup ? (
+        <div className="popup">
+          <div className="popup_inner">
+            <h2>Success!</h2>
+            <button className="close" onClick={togglePopup}>
+              Close me
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
